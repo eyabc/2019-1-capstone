f1a8c672d870662ba80e5daee9b603cc77368163
eyabc <bey4314@naver.com>
2019-03-11 15:23:48 +0900

    #16 초기세팅 frontend

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,25 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+const view = './views'
+
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+  {
+    path: '',
+    redirect: '/map'
+  },
+  {
+    path: '/map/',
+    component: {
+       default: () => import(`${view}/main.vue`),
+       leftAside:() => import(`${view}/leftAside.vue`),
+       rightAside: () => import(`${view}/rightAside.vue`)
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    children: [
+    ]
+  },
   ]
 })
---
+++ frontend/src/views/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="site-wrap">
+    <siteHeader />
+    <div class="container">
+      <div class="container-innner">
+        <router-view></router-view>
+      </div>
+    </div>
+    <router-view class="leftAside" name="leftAside" />
+    <router-view class="rightAside" name="rightAside"/>
+    <siteFooter />
+  </div>
+</template>
+<script>
+  import siteHeader from '@/components/header'
+  import siteFooter from '@/components/footer'
+  export default {
+    components: {
+      siteHeader, 
+      siteFooter,
+    }
+  }
+</script>
