056c7450a445410719e7d7e937469d8bd9c8619c
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-12 21:26:27 +0900

    #16 main.js fetch&nl2br (#23)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,18 @@ import store from './store'
 
 
 Vue.config.productionTip = false
+// # 16 ~~
+Vue.prototype.nl2br = str => {
+	return str.split("\n").join('<br>')
+}
+
+Vue.fetch = async (url, obj = {} => {
+	return await fetch(url, obj).then(res => res.json()).then(json => {
+		if (json.success) return json
+		else console.log(json.err)
+	})
+})
+// await this.fetch('/api/member') ~~~
 
 /*
 Vue.prototype.nl2br = str => {
