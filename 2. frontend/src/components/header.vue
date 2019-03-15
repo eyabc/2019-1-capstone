2e9567d6df88e6564afdb7bae2ccc216026e8ac4
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-15 20:42:57 +0900

    #26 header.vue frontend OK (#83)

---
+++ 2. frontend/src/components/header.vue
@@ -0,0 +1,37 @@
+<!-- #26 -->
+<template>
+  <div class="site-header">
+    <div>
+      <router-link to="/"><img class="site-logo" src="../assets/image/Logo.png" alt='Logo'></router-link>
+    </div>
+    <div class="header-search-bar">
+      <form @submit.prevent="search">
+        <input type="text" name="place" placeholder="위치를 입력해주세요">
+        <div class="header-search-plus" v-if="hover" >
+          <p><input type="" name="nameInplace" placeholder="현재 위치에서 검색할 그룹 이름"></p>
+          <p><input type="" name="nameAll" placeholder="검색할 그룹 이름"></p>
+          <p><input type="" name="theme" placeholder="검색할 주제"></p>
+        </div>
+        <input type="submit" name="">
+      </form>
+      <span @mouseover="hover = true" @mousedown="hoverToggle" class="search-plus-icon"><i class="fas fa-pencil-alt"></i></span>
+    </div>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+  data() {
+    return {
+      hover: false,
+    }
+  },
+  methods: {
+    hoverToggle () {
+      this.hover = !this.hover
+    },
+    search(){
+
+    }
+  }
+}
+</script>
