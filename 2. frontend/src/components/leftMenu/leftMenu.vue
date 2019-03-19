fa5e743443f87b588e77e76a25d7a51dcebf7d3b
eyabc <bey4314@naver.com>
2019-03-19 14:49:03 +0900

    중간저장

--- 2. frontend/src/components/leftMenu/leftMenu.vue
+++ 2. frontend/src/components/leftMenu/leftMenu.vue
@@ -1,8 +1,23 @@
 <template>
 	<div class="left-menu-wrap">
 		dfsd
+		<span class="folding " @click="foldingToggle">
+			<span class="folding-arrow"  v-if="!folding">></span>
+			<span class="folding-arrow"  v-else><</span>
+		</span>
 	</div>
 </template>
 <script type="text/javascript">
-	
+  export default {
+  data() {
+    return {
+      folding: false,
+    }
+  },
+  methods: {
+  	foldingToggle () {
+  		this.folding = ! this.folding
+  	}
+  }
+}
 </script>
