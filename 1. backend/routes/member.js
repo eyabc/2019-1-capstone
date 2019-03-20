6d0edca507b0e3ff3a12df91b1bfd24766ecae1a
Ryudain <rdi0526@naver.com>
2019-03-20 14:56:18 +0900

    #45 회원가입 backend

--- 1. backend/routes/member.js
+++ 1. backend/routes/member.js
@@ -14,6 +14,18 @@ router.post('/api/member/signin', async (req, res) => {
 	res.json(resultJSON)
 })
 
+// #45
+router.post('/api/member/signup', async(req,res) => {
+	const sql = 'insert into member (email, password, nickname, place) values(?,?,?,?)'
+	const resultJSON = {success: true}
+	try {
+		await execQuery(sql, [req.body.email, req.body.password, req.body.nickname, req.body.place])
+	} catch (error) {
+		resultJSON.success = false
+	}
+	res.json(resultJSON)
+})
+
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
