a5ccee6352b1f6e09cc294b3be09b2502ea695e5
JangHyun <ttl957@naver.com>
2019-03-20 14:24:42 +0900

    #79 delete backend

--- 1. backend/routes/member.js
+++ 1. backend/routes/member.js
@@ -1,9 +1,22 @@
+const execQuery = require('./db.js')
 var express = require('express');
 var router = express.Router();
+// #79
+router.delete('/api/member', async (req, res) => {
+	const sql = 'delete from member where idx = ?'
+	const resultJSON = {success: true}
+	try {
+		await execQuery(sql, [req.body.idx])
+	} catch (error){
+		resultJSON.success = false
+	}
+	res.json(resultJSON)
+})
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
