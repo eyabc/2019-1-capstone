bf4c413fa56816f2fe83e021700b55fcbec80715
Ryudain <rdi0526@naver.com>
2019-03-20 14:24:58 +0900

    #41 로그인 backend

--- 1. backend/routes/member.js
+++ 1. backend/routes/member.js
@@ -1,9 +1,22 @@
+const execQuery = require('./db.js')
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// #41 
+router.post('/api/member/signin', async (req, res) => {
+	const sql = 'select * from member where email = ? and password = ?'
+	const resultJSON = {success: true}
+	try {
+		resultJSON[member] = await execQuery(sql, [req.body.email, req.body.password])
+	} catch (error) {
+		resultJSON.success = false
+	}
+	res.json(resultJSON)
+})
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
