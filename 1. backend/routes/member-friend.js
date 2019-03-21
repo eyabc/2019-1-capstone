500883a0972ce1b1992d7cf31330bb45eb3535a5
JangHyun <ttl957@naver.com>
2019-03-22 00:19:37 +0900

    #63 친구삭제backend

--- 1. backend/routes/member-friend.js
+++ 1. backend/routes/member-friend.js
@@ -1,4 +1,28 @@
+const execQuery = require('./db.js')
 express = require('express');
 var router = express.Router();
 
+// #68
+router.delete('/api/member-friend', async (req, res) => {
+	const sql = 'insert into member-friend (midx, friend) value(?,?)'
+	const resultJON = {success: true}
+	try {
+		await execQuery(sql, [req.body.midx, req.body.friend])
+	} catch (error){
+		resultJON.success = false
+	}
+	res.json(resultJON)
+})
+
+// #63
+router.delete('/api/member-friend', async (req, res) => {
+	const sql = 'delete from member where midx=? and friend=?'
+	const resultJON = {success: true}
+	try {
+		await execQuery(sql, [req.body.midx, req.body.friend])
+	} catch (error){
+		resultJON.success = false
+	}
+	res.json(resultJON)
+})
 module.exports = router;
