383c1facba84a12f5dfc0ffcd47b816578c88b2d
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-22 18:11:23 +0900

    #28 (#108)
    * #46 html & css 하는중
    
    * #46 회원가입페이지 html&css"
    
    * #46 googlemap cdn 추가만함
    
    * #28 구글 지도 추가 완료

--- 2. frontend/src/components/leftMenu/notMember/login.vue
+++ 2. frontend/src/components/leftMenu/notMember/login.vue
@@ -15,7 +15,6 @@
 						<span class="pre"><i class="fas fa-key"></i></span>
 						<input type="password" name="password" class="full-width" required>
 						<span class="lbl">비밀번호</span>
-						<small v-if="capsLock">capsLock이 켜져있습니다</small>
 					</label>
 				</li>
 			</ul>
---
+++ 2. frontend/src/components/map/GoogleMapLoader.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="map-content">
+		<GmapMap class='map-wrap'
+		:center="{lat:10, lng:10}"
+		:zoom="7"
+		map-type-id="terrain"
+
+		>
+
+
+	</GmapMap>
+</div>
+</template>
+<script>
+	export default {
+		
+	};
+</script>
+
