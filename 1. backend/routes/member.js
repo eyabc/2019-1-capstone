acdb80203da1ad042fbda441ee02e7b5ef44e5cf
JangHyun <ttl957@naver.com>
2019-03-23 16:21:38 +0900

    #80 회원정보수정backend

--- 1. backend/routes/member.js
+++ 1. backend/routes/member.js
@@ -13,6 +13,20 @@ router.delete('/api/member', async (req, res) => {
 	res.json(resultJSON)
 })
 
+// #80
+router.update('api/member', async (req, res) => {
+	const sql = 'update member'
+				'set email = 'value1', password = 'value2', nickname = 'value3', place = 'value4''
+				'where idx = 1'
+	const resultJSON = {success: true}
+	try {
+		await execQuery(sql, [req.body.email, req.body.password, req.body.nickname, req.body.place])
+	} catch (error){
+		resultJSON.success = false
+	}
+	res.json(resultJSON)
+})
+
 
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
