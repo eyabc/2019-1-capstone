bbe8c959dc3937d93034a03a579591ebffda0a9e
Ryudain <rdi0526@naver.com>
2019-03-23 13:00:53 +0900

    #31 채팅방 이름 검색 backend

--- 1. backend/routes/chatting_group.js
+++ 1. backend/routes/chatting_group.js
@@ -1,4 +1,23 @@
+const execQuery = require('./db.js')
 var express = require('express');
 var router = express.Router();
+
+// #31
+router.post('/api/search-group', async (req, res) => {
+	const sql = 'select * from chatting_group where name = ?'
+	const resultJSON= {success: true}
+	try {
+		resultJSON[chatting_group] = await execQuery(sql, [req.body.name])
+	} catch (error) {
+		resultJSON.success = false
+	}
+	res.json(resultJSON)
+})
+	
+
+
+
+
+
 module.exports = router;
 
