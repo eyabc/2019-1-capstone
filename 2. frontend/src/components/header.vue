e37b7a5153293f294943205253daa24be470e154
eyabc <bey4314@naver.com>
2019-03-25 23:36:52 +0900

    #119 위치검색창 등록 완료

--- 2. frontend/src/components/header.vue
+++ 2. frontend/src/components/header.vue
@@ -4,19 +4,20 @@
     <h3 class="logo"><router-link to="/"><img class="site-logo" src="../assets/image/Logo.png" alt='Logo'></router-link></h3>
     <div class="header-search-bar">
       <form @submit.prevent="search">
-        <input type="text" name="place" placeholder="위치를 입력해주세요">
-        <input type="submit" name="">
+        <input type="text" id="pac-input" name="place" placeholder="위치를 입력해주세요">
+        <input type="submit">
         <span @mousedown="hoverToggle" class="search-plus-icon"><i class="fas fa-pencil-alt"></i></span>
         <div class="header-search-plus" v-if="hover" >
-          <p><input type="" name="nameInplace" placeholder="현재 위치에서 검색할 그룹 이름"></p>
-          <p><input type="" name="nameAll" placeholder="검색할 그룹 이름"></p>
-          <p><input type="" name="theme" placeholder="검색할 주제"></p>
+          <p><input type="text" name="nameInplace" placeholder="현재 위치에서 검색할 그룹 이름"></p>
+          <p><input type="text" name="nameAll" placeholder="검색할 그룹 이름"></p>
+          <p><input type="text" name="theme" placeholder="검색할 주제"></p>
         </div>
       </form>
     </div>
   </div>
 </template>
 <script type="text/javascript">
+  import eventBus from '@/eventBus'
   export default {
   data() {
     return {
@@ -28,7 +29,6 @@
       this.hover = !this.hover
     },
     search(){
-
     }
   }
 }
