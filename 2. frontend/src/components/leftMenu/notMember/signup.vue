195597a5626ed64d5859921a31eb1381cc55db6c
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-25 23:41:25 +0900

    #119 (#121)
    * #119 위치 검색 완료
    
    * #119 위치검색창 등록 완료

--- 2. frontend/src/components/leftMenu/notMember/signup.vue
+++ 2. frontend/src/components/leftMenu/notMember/signup.vue
@@ -1,55 +1,54 @@
 <template>
 	<div class="signup-wrap" v-if="!$store.getters.member">
 		<p class="notMember-logo">{{this.$store.getters.title}}</p>
-		<form action="/api/signup" method="post" @submit.prevent="signup">
-			<ul class="fields">
-				<li>
-					<label class="input-label">
-						<span class="pre"><i class="fas fa-envelope"></i></span>
-						<input type="text" name="email" class="full-width" required>
-						<span class="lbl">이메일</span>
-					</label>
-				</li>
-				<li>
-					<label class="input-label">
-						<span class="pre"><i class="fas fa-key"></i></span>
-						<input type="password" name="password" class="full-width" required>
-						<span class="lbl">비밀번호</span>
-					</label>
-				</li>
-				<li>
-					<label class="input-label">
-						<span class="pre"><i class="fas fa-key"></i></span>
-						<input type="password" name="password-re" class="full-width" required>
-						<span class="lbl">비밀번호 재확인</span>
-					</label>
-				</li>
-				<li>
-					<label class="input-label">
-						<span class="pre"><i class="fas fa-user"></i></span>
-						<input type="text" name="email" class="full-width" required>
-						<span class="lbl">닉네임</span>
-					</label>
-				</li>
-				<li>
-					<label class="input-label">
-						<span class="pre"><i class="fas fa-map-marker-alt"></i></span>
-						<input type="text" name="email" class="full-width" required>
-						<span class="lbl">위치</span>
-					</label>
-				</li>
-				<input class="login-btn btn" type="submit" value="완료">
-				<input class="login-btn back-btn" type="button" value="로그인 페이지로 이동" @click.prevent="goLogin">
-
-			</ul>
-		</form>
+		<ul class="fields">
+			<li>
+				<label class="input-label">
+					<span class="pre"><i class="fas fa-envelope"></i></span>
+					<input type="text" name="email" class="full-width" required>
+					<span class="lbl">이메일</span>
+				</label>
+			</li>
+			<li>
+				<label class="input-label">
+					<span class="pre"><i class="fas fa-key"></i></span>
+					<input type="password" name="password" class="full-width" required>
+					<span class="lbl">비밀번호</span>
+				</label>
+			</li>
+			<li>
+				<label class="input-label">
+					<span class="pre"><i class="fas fa-key"></i></span>
+					<input type="password" name="password-re" class="full-width" required>
+					<span class="lbl">비밀번호 재확인</span>
+				</label>
+			</li>
+			<li>
+				<label class="input-label">
+					<span class="pre"><i class="fas fa-user"></i></span>
+					<input type="text" name="email" class="full-width" required>
+					<span class="lbl">닉네임</span>
+				</label>
+			</li>
+			<li>
+				<label class="input-label">
+					<span class="pre"><i class="fas fa-map-marker-alt"></i></span>
+					<input type="text" name="place" ref="place" class="full-width" required placeholder="위치를 입력해주세요">
+				</label>
+			</li>
+			<input class="login-btn btn" type="submit" value="완료">
+			<input class="login-btn back-btn" type="button" value="로그인 페이지로 이동" @click.prevent="goLogin">
+		</ul>
 	</div>
 </template>
 <script type="text/javascript">
-
+	import eventBus from '@/eventBus'
 	export default {
 		methods : {
 			goLogin () { this.$store.commit('leftMenu', 'login')}
+		},
+		mounted () {
+			eventBus.initSearchBox(this.$refs.place)
 		}
 	}
 </script>
