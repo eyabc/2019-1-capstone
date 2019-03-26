f4d3584923921e0929370957e9e20a9082477358
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-26 18:01:04 +0900

    #46 회원가입 완료 (#124)
    * #46 html & css 하는중
    
    * #46 회원가입페이지 html&css"
    
    * #46 googlemap cdn 추가만함
    
    * #46 ??
    
    * #46 유효성 검사 만드는 중
    
    * #46 회원가입 폼검증 완료 위치 경도위도 get 해야함
    
    * #46 회원가입 완료

--- 2. frontend/src/components/leftMenu/notMember/signup.vue
+++ 2. frontend/src/components/leftMenu/notMember/signup.vue
@@ -6,7 +6,9 @@
 				<li>
 					<label class="input-label">
 						<span class="pre"><i class="fas fa-envelope"></i></span>
-						<input type="text" v-model="data.email" class="full-width" required @focus="status.email=''" @blur="overlapEmail">
+						<input type="text" class="full-width"
+						v-model="data.email"
+						required />
 						<span class="lbl">이메일</span>
 						<p class="check-show">{{ statusEmail }}</p>
 					</label>
@@ -24,7 +26,7 @@
 					<label class="input-label">
 						<span class="pre"><i class="fas fa-key"></i></span>
 						<input type="password" name="password-re" class="full-width" required
-						v-model="data.password_re"						>
+						v-model="password_re">
 						<span class="lbl">비밀번호 재확인</span>
 						<p class="check-show">{{ statusPassword_re }}</p>
 					</label>
@@ -32,15 +34,15 @@
 				<li>
 					<label class="input-label">
 						<span class="pre"><i class="fas fa-user"></i></span>
-						<input type="text" name="email" class="full-width" required>
+						<input type="text" name="email" class="full-width" required v-model="data.nickname">
 						<span class="lbl">닉네임</span>
 					</label>
 				</li>
 				<li>
 					<label class="input-label">
 						<span class="pre"><i class="fas fa-map-marker-alt"></i></span>
-						<input type="text" v-model="place" name="place" ref="place" class="full-width" required placeholder="위치를 입력해주세요" @keyup.enter="placeSubmit"> 
-						<p class="check-show">{{ place }}</p>
+						<input type="text" v-model="data.place" name="place" ref="place" class="full-width" required placeholder="위치를 입력해주세요" @keydown.enter.prevent="placeSubmit"> 
+						<p class="check-show">{{ data.place }}</p>
 					</label>
 				</li>
 				<input class="login-btn btn" type="submit" value="완료">
@@ -54,26 +56,34 @@
 	export default {
 		data () {
 			return {
-				place: '',
-				password: '',
-				status: {
-					email: '',
-					password: '특수문자, 문자, 숫자 포함 형태의 8~15자리 이내',
-					password_re: '',
-					nickname: '',
-				},
+				password_re: '',
 				data: {
 					email: '',
 					password: '',
-					password_re: '',
-				}
+					nickname: '',
+					place: '',
+					lat: '',
+					lng: '',
+				},
+				overlapEmailResult: '',
 			}
 		},
 		computed: {
 			statusEmail () {
 				const email = this.data.email
 				const regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-				return email === '' || email.match(regex) ? '' : '유효하지 않은 이메일 형식 입니다.'
+				let result = ''
+				if (email !== '') {
+					if (!email.match(regex)) {
+						result = '유효하지 않은 이메일 형식입니다.'
+					} else {
+						this.overlapEmail(email)	
+						if (this.overlapEmailResult !== '') {
+							result = this.overlapEmailResult
+						}
+					}
+				}
+				return result
 			},
 			statusPassword () {
 				const password = this.data.password
@@ -81,35 +91,58 @@
 				return password === '' || password.match(regex) ? '특수문자, 문자, 숫자 포함 형태의 8~15자리 이내' : '비밀번호 형식이 틀렸습니다.'
 			},
 			statusPassword_re () {
-				const password_re = this.data.password_re
-				return password_re === '' ? '' : password_re.match(this.data.password) ? '비밀번호가 일치합니다.' : '비밀번호가 일치하지 않습니다.'
+				const password_re = this.password_re
+				return password_re === '' ? '' : password_re === this.data.password ? '비밀번호가 일치합니다.' : '비밀번호가 일치하지 않습니다.'
 			},
 
-			statusAll () {
-
-			}
 		},
 
 		mounted () {
+			eventBus.initLocation(this)
 			eventBus.initSearchBox(this.$refs.place)
+			this.$on('getLocation', location => {
+				this.data.lat = location.lat
+				this.data.lng = location.lng
+			})
 		},
 		methods: {
 			goLogin () { this.$store.commit('leftMenu', 'login')},
-			placeSubmit (e) { this.place=e.target.value },
-			signup () {
-				if (staus)
-					if(json.success){
-						alert('회원가입이 완료되었습니다.')
-						this.$store.commit('leftMenu', 'login')
-					} else {
-						alert('양식을 다시 확인해 주세요')
-					}
+			placeSubmit (e) { this.data.place=e.target.value },
+			signup (e) {
+				if(!this.statusAll()) {
+					alert('양식을 다시 확인해 주세요') 
+				} else {
+					const frm = e.target
+					const data = this.data
+					this.$fetch('/api/member/signup', {
+						method: 'post',
+						headers: {'Content-Type':'application/json'},
+						body: JSON.stringify({ data })
+					}).then(json => {
+						if(json.msg === false) {
+							alert('회원가입 완료')
+							this.$store.commit("leftMenu", 'login')
+						} else {
+							alert('회원가입 실패')
+						}
+					})
 				}
 			},
-			async overlapEmail (e) {
-				const email = e.target.value
-				fetch('')
+			overlapEmail (email) {
+				const json = this.$fetch('/api/member/signup/email', {
+					method: 'post',
+					headers: {'Content-Type':'application/json'},
+					body: JSON.stringify({ email })
+				}).then(json => {
+					this.overlapEmailResult = !json.email ? '이미 사용중인 이메일입니다.' : ''
+				})
+			},
+			statusAll () {
+				if (this.statusEmail === '' && this.statusPassword === '특수문자, 문자, 숫자 포함 형태의 8~15자리 이내' && this.statusPassword_re === '비밀번호가 일치합니다.') {
+					return true
+				}
 			}
 		}
+	}
 
-	</script>
+</script>
