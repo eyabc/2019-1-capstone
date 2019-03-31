38e364ee3fc4ee9e1aef9f7ce493c06c7eb45dbe
박은영 <31977543+eyabc@users.noreply.github.com>
2019-03-31 17:45:16 +0900

    #134 (#137)
    * #134 중간저장
    
    * #134 친구 위치정보 조회

--- 2. frontend/src/eventBus.js
+++ 2. frontend/src/eventBus.js
@@ -5,19 +5,19 @@ const bus = new Vue({
 		map: null,
 		locationTarget: null
 	},
-  methods: {
-	  initMap(currentLocation) {
-	    if(currentLocation.lat === 0 || currentLocation.lng === 0) {
-	      currentLocation = {lat: -33.8688, lng: 151.2195}
-	    }
-	    this.map = new google.maps.Map(document.getElementById('map'), {
-	      center: {lat:currentLocation.lat, lng:currentLocation.lng},
-	      zoom: 13,
-	      mapTypeId: 'roadmap'
-	    });
-	    this.initSearchBox(document.getElementById('pac-input'))
-	  },
-	  initSearchBox (input) {
+	methods: {
+		initMap(currentLocation) {
+			if(currentLocation.lat === 0 || currentLocation.lng === 0) {
+				currentLocation = {lat: -33.8688, lng: 151.2195}
+			}
+			this.map = new google.maps.Map(document.getElementById('map'), {
+				center: {lat:currentLocation.lat, lng:currentLocation.lng},
+				zoom: 13,
+				mapTypeId: 'roadmap'
+			});
+			this.initSearchBox(document.getElementById('pac-input'))
+		},
+		initSearchBox (input) {
 	    // Create the search box and link it to the UI element.
 	    const map = this.map
 	    const searchBox = new google.maps.places.SearchBox(input);
@@ -27,8 +27,8 @@ const bus = new Vue({
 	    let markers = []
 	    const bounds_changed = () => { searchBox.setBounds(map.getBounds()); }
 	    const places_changed = () => {
-	      const places = searchBox.getPlaces()
-	      if (places.length == 0) return;
+	    	const places = searchBox.getPlaces()
+	    	if (places.length == 0) return;
 	      // Clear out the old markers.
 	      markers.forEach(marker => marker.setMap(null))
 	      markers = [];
@@ -36,28 +36,53 @@ const bus = new Vue({
 	      // For each place, get the icon, name and location.
 	      const bounds = new google.maps.LatLngBounds();
 	      places.forEach(place => {
-		      if (!place.geometry) return;
-		      const icon = { url: place.icon, size: new google.maps.Size(71, 71), origin: new google.maps.Point(0, 0), anchor: new google.maps.Point(17, 34), scaledSize: new google.maps.Size(25, 25) };
+	      	if (!place.geometry) return;
+	      	const icon = { url: place.icon, size: new google.maps.Size(71, 71), origin: new google.maps.Point(0, 0), anchor: new google.maps.Point(17, 34), scaledSize: new google.maps.Size(25, 25) };
 		      // Create a marker for each place.
 		      markers.push(new google.maps.Marker({map, icon, title: place.name, position: place.geometry.location}))
 		      if (this.locationTarget !== null) {
-			      const location = place.geometry.location
+		      	const location = place.geometry.location
 		      	this.locationTarget.$emit('getLocation', {
 		      		lat: location.lat(),
 		      		lng: location.lng()
 		      	})
 		      }
 		      place.geometry.viewport ? bounds.union(place.geometry.viewport) : bounds.extend(place.geometry.location)
-	      });
+		  });
 	      map.fitBounds(bounds);
-	    }
-	    map.addListener('bounds_changed', bounds_changed);
-	    searchBox.addListener('places_changed', places_changed);
-	  },
-	  initLocation (target) {
-	  	this.locationTarget = target
 	  }
-  }
+	  map.addListener('bounds_changed', bounds_changed);
+	  searchBox.addListener('places_changed', places_changed);
+	},
+	initLocation (target) {
+		this.locationTarget = target
+	},
+	setLocation(Location, data) {
+		this.map.setZoom(18);
+		this.map.setCenter({lat: Location.lat, lng: Location.lng}); 
+		var marker = new google.maps.Marker({
+			position: Location,
+			map: this.map,
+			title: "hle"
+		});
+		var contentString = '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h3 id="firstHeading" class="firstHeading">'+data.nickname+'</h3>'+
+		'<div id="bodyContent">'+
+		'<p>'+data.place+'</p>'+
+		'</div>'+
+		'</div>';
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+		infowindow.open(this.map, marker);
+		marker.addListener('click', function() {
+			infowindow.open(this.map, marker);
+		});
+
+	},
+}
 })
 
 
