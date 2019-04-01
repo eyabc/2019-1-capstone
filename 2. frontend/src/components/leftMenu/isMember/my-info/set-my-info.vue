57e3b11324fadd22e21bd40f7b3021eff38bf657
eyabc <bey4314@naver.com>
2019-04-01 22:00:15 +0900

    #80 css html update

--- 2. frontend/src/components/leftMenu/isMember/my-info/set-my-info.vue
+++ 2. frontend/src/components/leftMenu/isMember/my-info/set-my-info.vue
@@ -1,139 +1,170 @@
 <template>
-	<div class="member-info-wrap" @click="">		
+  <div class="member-info-wrap" @click="">		
     <ul class="member-info-btn">
       <li><a href="#" @click.prevent=""># 내 정보 수정</a></li>
       <li><a href="#" @click.prevent=""># 공개 설정</a></li>
+      <li><a href="#" @click.prevent="back">수정 취소</a></li>
     </ul>
     <form class="member-info-set" method="post" @submit.prevent='setMember'>
-     <div id="image-set-img">
-      <a class="image-set-small-img image-set-img-item " @click="toggleShow" :class="{ none : imgDataUrl !=='' }" >프로필 사진 선택 <br/> jpg/png </a>
-      <img class="image-set-small-img img" :src="imgDataUrl" >
-      <my-upload field="img" 
-      @crop-success="cropSuccess"
-      @crop-upload-success="cropUploadSuccess"
-      @crop-upload-fail="cropUploadFail"
-      v-model="show"
-      :width="500"
-      :height="500"
-      lang-type="en"
-      imgBgc="#000"
-      img-format="png" ></my-upload>
-    </div>
-    <!-- <img class="image-set-small-img"src="./default-avatar.png" alt="img" /> -->
-    <div class="member-info-desc re-width">
-      <ul class="visibility">
-        <li>
-         <span>1 : 1 대화 허용</span>
-         <span class="checks etrans">
-          <input type="checkbox" id="cb1" name="visibility_oneChat">
-          <label for="cb1"></label>
-        </span>
-      </li>
-      <li>
-       <span>위치 공개</span>
-       <span class="checks etrans">
-        <input type="checkbox" id="cb2" name="visibility_place"> 
-        <label for="cb2"></label>
-      </span>
-    </li>
-  </ul>
-  <ul class="fields">
-   <li>
-     <label class="input-label">
-      <span class="pre"><i class="fas fa-envelope"></i></span>
-      <input type="text" class="full-width input-disabled" disabled :value="member.email"/>
-      <span class="lbl"></span>
-      <p class="check-show" ></p>
-    </label>
-  </li>
-  <li>
-   <label class="input-label">
-    <span class="pre"><i class="fas fa-key"></i></span>
-    <input type="password" name="password" class="full-width" placeholder="비밀번호">
-  </label>
-</li>
-<li>
- <label class="input-label">
-  <span class="pre"><i class="fas fa-key"></i></span>
-  <input type="password" name="password-re" class="full-width" required placeholder="비밀번호 재확인">
-</label>
-</li>
-<li>
- <label class="input-label">
-  <span class="pre"><i class="fas fa-user"></i></span>
-  <input type="text" name="email" class="full-width" required :value="member.nickname">
-  <span class="lbl">닉네임</span>
-</label>
-</li>
-<li>
- <label class="input-label">
-  <span class="pre"><i class="fas fa-map-marker-alt"></i></span>
-  <input type="text"  name="place" ref="place" class="full-width" required placeholder="위치를 입력해주세요" @keydown.enter.prevent="" :value="member.place"> 
-  <p class="check-show">{{  }}</p>
-</label>
-</li>
-<input class="login-btn btn" type="submit" value="수정 완료">
-</ul>
-</div>
-</form>
-</div>
+      <div id="image-set-img">
+        <a class="image-set-small-img image-set-img-item " @click="toggleShow" :class="{ none : imgDataUrl !=='' }" >프로필 사진 선택 <br/> jpg/png </a>
+        <img class="image-set-small-img img" :src="imgDataUrl" >
+        <my-upload field="img" 
+        @crop-success="cropSuccess"
+        @crop-upload-success="cropUploadSuccess"
+        @crop-upload-fail="cropUploadFail"
+        v-model="show"
+        :width="500"
+        :height="500"
+        lang-type="en"
+        imgBgc="#000"
+        img-format="png" ></my-upload>
+        <br/><p class="dashed-btn"><a href="#" @click.prevent="initImage">이미지 삭제</a></p>
+      </div> 
+      <!-- <img class="image-set-small-img"src="./default-avatar.png" alt="img" /> -->
+      <div class="member-info-desc re-width">
+        <ul class="visibility">
+          <li>
+            <span>1 : 1 대화 허용</span>
+            <span class="checks etrans">
+              <input type="checkbox" id="cb1" ref="oneChat" name="visibility_oneChat">
+              <label for="cb1"></label>
+            </span>
+          </li>
+          <li>
+            <span>위치 공개</span>
+            <span class="checks etrans">
+              <input type="checkbox" id="cb2" ref="showPlace"name="visibility_place"> 
+              <label for="cb2"></label>
+            </span>
+          </li>
+        </ul>
+        <ul class="fields">
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-envelope"></i></span>
+              <input type="text" class="full-width input-disabled" disabled :value="member.email"/>
+              <span class="lbl"></span>
+              <p class="check-show" ></p>
+            </label>
+          </li>
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-key"></i></span>
+              <input type="password" name="password" class="full-width" placeholder="현재 비밀번호를 입력해야 정보를 수정할 수 있습니다." required>
+            </label>
+          </li>
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-key"></i></span>
+              <input type="password" name="password" class="full-width" placeholder="새 비밀번호">
+            </label>
+          </li>
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-key"></i></span>
+              <input type="password" name="password-re" class="full-width" required placeholder="새 비밀번호 재확인">
+            </label>
+          </li>
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-user"></i></span>
+              <input type="text" name="email" class="full-width" required :value="member.nickname">
+              <span class="lbl">닉네임</span>
+            </label>
+          </li>
+          <li>
+            <label class="input-label">
+              <span class="pre"><i class="fas fa-map-marker-alt"></i></span>
+              <input type="text"  name="place" ref="place" class="full-width" required placeholder="위치를 입력해주세요" @keydown.enter.prevent="" :value="member.place"> 
+              <p class="check-show">{{  }}</p>
+            </label>
+          </li>
+          <input class="login-btn btn" type="submit" value="수정 완료">
+        </ul>
+      </div>
+    </form>
+  </div>
 </template>
 <script type="text/javascript">
-	import 'babel-polyfill'; // es6 shim
-	import myUpload from 'vue-image-crop-upload';
+import 'babel-polyfill'; // es6 shim
+import myUpload from 'vue-image-crop-upload';
 
-	export default {
-		data() {
-			return {
-				show: false,
-				imgDataUrl: '', // the datebase64 url of created image
-        member: this.$store.state.member
-      }
+export default {
+  data() {
+    return {
+      show: false,
+      imgDataUrl: '', // the datebase64 url of created image
+      member: this.$store.state.member
+    }
+  },
+  components: {
+    'my-upload': myUpload
+  },
+  mounted() {
+    console.log(this.member.place_visibility)
+    if(this.member.one_chat_available === 1){ 
+      this.$refs.oneChat.checked = true
+    }  
+    if(this.member.place_visibility === 1) {
+      this.$refs.showPlace.checked = true
+    }
+    this.member.profile_img === '' ? this.imgDataUrl = '' : this.member.profile_img  
+  },
+  methods: {
+    setMember() {
+
     },
-    components: {
-     'my-upload': myUpload
-   },
-   methods: {
-     setMember() {
-
-     },
-     toggleShow() {
+    toggleShow() {
       this.show = !this.show;
     },
-            /**
-			 * crop success
-			 *
-			 * [param] imgDataUrl
-			 * [param] field
-			 */
-			 cropSuccess(imgDataUrl, field){
-			 	console.log('-------- crop success --------');
-			 	this.imgDataUrl = imgDataUrl;
-			 	console.log(imgDataUrl)
-			 },
-			/**
-			 * upload success
-			 *	
-			 * [param] jsonData  server api return data, already json encode
-			 * [param] field
-			 */
-			 cropUploadSuccess(jsonData, field){
-			 	console.log('-------- upload success --------');
-			 	console.log(jsonData);
-			 	console.log('field: ' + field);
-			 },
-			/**
-			 * upload fail
-			 *
-			 * [param] status    server api return error status, like 500
-			 * [param] field
-			 */
-			 cropUploadFail(status, field){
-			 	console.log('-------- upload fail --------');
-			 	console.log(status);
-			 	console.log('field: ' + field);
-			 }
+    initImage () {
+      this.imgDataUrl = ''
+    },
+    back () {
+      this.$store.commit('isMember', 'myInfo')
+    },
+/**
+* crop success
+*
+* [param] imgDataUrl
+* [param] field
+*/
+cropSuccess(imgDataUrl, field){
+  console.log('-------- crop success --------');
+  this.imgDataUrl = imgDataUrl;
+  console.log(imgDataUrl)
+},
+/**
+* upload success
+*	
+* [param] jsonData  server api return data, already json encode
+* [param] field
+*/
+cropUploadSuccess(jsonData, field){
+  console.log('-------- upload success --------');
+  console.log(jsonData);
+  console.log('field: ' + field);
+},
+/**
+* upload fail
+*
+* [param] status    server api return error status, like 500
+* [param] field
+*/
+cropUploadFail(status, field){
+  console.log('-------- upload fail --------');
+  console.log(status);
+  console.log('field: ' + field);
+}
 
-			}
-		}
-	</script>
+}
+}
+</script>
+<style>
+.dashed-btn {
+  width: 150px;
+  margin-left: 10px;
+  font-size: 12px;
+}
+</style>
